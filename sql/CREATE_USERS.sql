61dac0a1bfac873c86b44a675325a2a841b469e9
yuNaganuma2 <s09ti050@gmail.com>
2017-06-14 00:18:56 +0900

    USERSテーブルのCREATE文とINSERT文を作成しました

---
+++ sql/CREATE_USERS.sql
@@ -0,0 +1,16 @@
+﻿-- Project Name : kinyuu
+-- Date/Time    : 2017/06/14 0:06:46
+-- Author       : Yu
+-- RDBMS Type   : MySQL
+-- Application  : A5:SQL Mk-2
+
+-- ユーザー
+create table users (
+  ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY comment 'ID'
+  , username VARCHAR(50) comment 'ユーザー名'
+  , password VARCHAR(255) comment 'パスワード	 HASH値'
+  , role VARCHAR(20) comment 'ロール'
+  , created DATETIME default NULL comment '作成日時'
+  , modified DATETIME default NULL comment '更新日時'
+) comment 'ユーザー' ;
+
---
+++ sql/INSERT_USER.sql
@@ -0,0 +1,2 @@
+﻿-- ユーザー作成 user:testuser, password:password
+insert into users values ('1','testuser','$2y$10$Wrrc594T4bXpUEOLMj/X8.YGb/Cvf6hW2tXuLZBrnF6V7a2zHM1dm',NULL,NULL,NULL);
